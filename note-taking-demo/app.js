b9c6e8f89df885d6a40d2da691a9bc18b030a18a
Coral Xinting He <hxth2018@gmail.com>
2019-06-19 17:16:36 -0400

    [node-js]added ingredients filed

--- note-taking-demo/app.js
+++ note-taking-demo/app.js
@@ -1,5 +1,6 @@
 const dishManager = (() => {
     const init = () => {
+        console.log("initing...")
         document.getElementById("app-submit").addEventListener("click", _addADish);
         document
             .getElementById("app-text-input")
@@ -14,14 +15,16 @@ const dishManager = (() => {
      * @param {event} e
      */
     const _addADish = e => {
-
         e.preventDefault();
         const dishName = document.getElementById("app-text-input").value;
+        const dishIng = document.getElementById("app-ingredients-input").value;
 
-        if (dishName) {
-            const newDish = _createDish(dishName);
+
+        if (dishName && dishIng) {
+            const newDish = _createDish(dishName, dishIng);
             document.getElementById("app-output").appendChild(newDish);
             document.getElementById("app-text-input").value = "";
+            document.getElementById("app-ingredients-input").value = "";
 
         } else {
             _printError();
@@ -31,13 +34,25 @@ const dishManager = (() => {
     /**
      * Returns a dish note node
      */
-    const _createDish = (text) => {
+    const _createDish = (title, ingredients) => {
         // create 2 parts:
         // 1. note body
         // 2. delete handler
+        const ingArr = ingredients.split("\n");
+        const ingredientNodes = ingArr.map(item => document.createTextNode(item))
         let note = document.createElement("li");
-        const t = document.createTextNode(text);
-        note.appendChild(t);
+        const titleNode = document.createTextNode(title);
+        note.appendChild(titleNode);
+
+        let ingUl = document.createElement("ul");
+
+        ingredientNodes.forEach(node => {
+            let ingItem = document.createElement("li");
+            ingItem.appendChild(node);
+            ingUl.appendChild(ingItem)
+        })
+
+        note.appendChild(ingUl);
         note.classList.add("app-output-list");
 
         let deleteEle = document.createElement("span");
@@ -56,7 +71,7 @@ const dishManager = (() => {
     const _initDelete = dishObjs => {
         for (const dish of dishObjs) {
             dish.addEventListener("click", _deleteOneDish);
-        };
+        }
     };
 
     /**
